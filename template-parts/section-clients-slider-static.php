b61c1e43afa2c8e0c54d502668f7660bf0c00807
tarassych <taras.sych@gmail.com>
2018-06-29 01:10:42 +0300

    First Yuri theme updates
    has some minor code organization problems must be fixed, but it's still
    working

--- template-parts/section-clients-slider-static.php
+++ template-parts/section-clients-slider-static.php
@@ -1,16 +1,25 @@
 <section class="clients">
 	<div class="container no-overflow">
-		<div class="clients-slider-static">
+		<div class="clients-slider">
 			<?php
-			$args = array( 'posts_per_page' => 5, 'post_type' => 'clients' );
+			$args = array( 'posts_per_page' => -1, 'post_type' => 'clients' );
 			$clients = get_posts( $args );
 			if ( sizeof($clients) > 0 )
 			{
-				foreach( $clients as $client )
+				foreach( array_chunk($clients, 2) as $client )
 				{
 					?>
 					<div class="one-client-slide">
-						<img src="<?php echo get_post_meta($client->ID, 'wpcf-client-logo', true);?>">
+					
+					<div class="flip">
+					<?php foreach($client as $key => $cl) { ?>
+					<div class="<?php echo ($key == '0') ? 'front' : 'back'; ?>">
+						<img src="<?php echo get_post_meta($cl->ID, 'wpcf-client-logo', true);?>">
+						</div>
+						<?php } ?>
+
+					</div>
+					
 					</div>
 					<?php
 				}
